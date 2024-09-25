e868773aebc611c226693ae8c09e3a8ac7163ed4
Lưu Viết Công <congluu090502@gmail.com>
2024-09-25 22:43:00 +0700

    Add files via upload

---
+++ models/note_model.dart
@@ -0,0 +1,11 @@
+class Note{
+  final String title;
+  final String content;
+  final DateTime dateCreated;
+
+  Note({
+    required this.title,
+    required this.content,
+    required this.dateCreated
+});
+}
